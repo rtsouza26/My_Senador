060fcb6bb4856d3158d550aa753dd636d1752d1b
teteretu <teteretu@yahoo.com>
2018-08-12 16:02:08 -0300

    Funcionalidade de decriptar senhas, e salvá-las encriptadas no banco

--- src/main/java/br/com/mysenador/mysenador/controller/UsuarioController.java
+++ src/main/java/br/com/mysenador/mysenador/controller/UsuarioController.java
@@ -1,9 +1,24 @@
 package br.com.mysenador.mysenador.controller;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.DigestException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -37,8 +52,9 @@ public class UsuarioController {
 		Map<String,Object> map = new HashMap<>();
 		
 		try {
+	        
 			map = mapper.readValue(registerForm, Map.class);
-			user.setEmail(map.get("email").toString());
+			user.setEmail(decryptAES(map.get("email").toString()));
 			user.setSenha(map.get("password").toString());
 			user.setName(map.get("name").toString());
 			
@@ -61,13 +77,14 @@ public class UsuarioController {
 		Map<String,Object> map = new HashMap<>();
 		
 		try {
+	        
 			map = mapper.readValue(login, Map.class);
 			
-			user.setEmail(map.get("email").toString());
+			user.setEmail(decryptAES(map.get("email").toString()));
 			
 			User userTemp = userRep.findById(user.getEmail()).get();
 			
-			if (userTemp != null && userTemp.getSenha().equals(map.get("password").toString())) {
+			if (userTemp != null && decryptAES(userTemp.getSenha()).equals(decryptAES(map.get("password").toString()))) {
 				map.put("confirmLogin", true);
 				return mapper.writeValueAsString(map);
 			} else {
@@ -83,4 +100,87 @@ public class UsuarioController {
 		
 		
 	}
+	
+	private String decryptAES(String crypt) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		String secret = "Exadsgdhg12436!&@%";
+        
+		byte[] cipherData = Base64.decodeBase64(crypt);
+		byte[] saltData = Arrays.copyOfRange(cipherData, 8, 16);
+
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+		final byte[][] keyAndIV = GenerateKeyAndIV(32, 16, 1, saltData, secret.getBytes(StandardCharsets.UTF_8), md5);
+		SecretKeySpec key = new SecretKeySpec(keyAndIV[0], "AES");
+		IvParameterSpec iv = new IvParameterSpec(keyAndIV[1]);
+
+		byte[] encrypted = Arrays.copyOfRange(cipherData, 16, cipherData.length);
+		Cipher aesCBC = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		aesCBC.init(Cipher.DECRYPT_MODE, key, iv);
+		byte[] decryptedData = aesCBC.doFinal(encrypted);
+		String decryptedText = new String(decryptedData, StandardCharsets.UTF_8);
+
+		return decryptedText;
+	}
+	 
+	/**
+	 * Generates a key and an initialization vector (IV) with the given salt and password.
+	 * <p>
+	 * This method is equivalent to OpenSSL's EVP_BytesToKey function
+	 * (see https://github.com/openssl/openssl/blob/master/crypto/evp/evp_key.c).
+	 * By default, OpenSSL uses a single iteration, MD5 as the algorithm and UTF-8 encoded password data.
+	 * </p>
+	 * @param keyLength the length of the generated key (in bytes)
+	 * @param ivLength the length of the generated IV (in bytes)
+	 * @param iterations the number of digestion rounds
+	 * @param salt the salt data (8 bytes of data or <code>null</code>)
+	 * @param password the password data (optional)
+	 * @param md the message digest algorithm to use
+	 * @return an two-element array with the generated key and IV
+	*/
+	public static byte[][] GenerateKeyAndIV(int keyLength, int ivLength, int iterations, byte[] salt, byte[] password, MessageDigest md) {
+
+	    int digestLength = md.getDigestLength();
+	    int requiredLength = (keyLength + ivLength + digestLength - 1) / digestLength * digestLength;
+	    byte[] generatedData = new byte[requiredLength];
+	    int generatedLength = 0;
+
+	    try {
+	        md.reset();
+
+	        // Repeat process until sufficient data has been generated
+	        while (generatedLength < keyLength + ivLength) {
+
+	            // Digest data (last digest if available, password data, salt if available)
+	            if (generatedLength > 0)
+	                md.update(generatedData, generatedLength - digestLength, digestLength);
+	            md.update(password);
+	            if (salt != null)
+	                md.update(salt, 0, 8);
+	            md.digest(generatedData, generatedLength, digestLength);
+
+	            // additional rounds
+	            for (int i = 1; i < iterations; i++) {
+	                md.update(generatedData, generatedLength, digestLength);
+	                md.digest(generatedData, generatedLength, digestLength);
+	            }
+
+	            generatedLength += digestLength;
+	        }
+
+	        // Copy key and IV into separate byte arrays
+	        byte[][] result = new byte[2][];
+	        result[0] = Arrays.copyOfRange(generatedData, 0, keyLength);
+	        if (ivLength > 0)
+	            result[1] = Arrays.copyOfRange(generatedData, keyLength, keyLength + ivLength);
+
+	        return result;
+
+	    } catch (DigestException e) {
+	        throw new RuntimeException(e);
+
+	    } finally {
+	        // Clean out temporary data
+	        Arrays.fill(generatedData, (byte)0);
+	    }
+	}
+
 }
